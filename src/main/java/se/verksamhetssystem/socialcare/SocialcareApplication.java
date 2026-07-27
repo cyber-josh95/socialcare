b5c26310163b9db2ede27bef5348c47650286fb2
cyber-josh25 <joshwa.miscawi@outlook.com>
2026-07-27 21:55:44 +0200

    Initial commit: socialcare Spring Boot project

---
+++ src/main/java/se/verksamhetssystem/socialcare/SocialcareApplication.java
@@ -0,0 +1,13 @@
+package se.verksamhetssystem.socialcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/verksamhetssystem/socialcare/SocialcareApplicationTests.java
@@ -0,0 +1,13 @@
+package se.verksamhetssystem.socialcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
